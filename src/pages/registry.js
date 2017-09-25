2dc8ae95c895e2d08d3bfedc0d0047221aa63d11
hshelbur <howard.shelburne@gmail.com>
2017-09-25 14:17:32 -0400

    registry page created and routed to

---
+++ src/pages/registry.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Registry extends Component {
+	render() {
+		return(
+			<div className="registry vertical-align">
+				<h1>Megan is registered at</h1>
+				<a href="https://amazon.com" className="registry-link" target="_blank" rel="noopener noreferrer">AMAZON</a>
+			</div>
+		);
+	}
+}
+
+export default Registry

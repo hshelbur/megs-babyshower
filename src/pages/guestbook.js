c71215a5ccd270b93a999cc842cdae9bcae65317
hshelbur <howard.shelburne@gmail.com>
2017-09-29 14:01:15 -0400

    firebase database hooked up, sending and receiving data.

--- src/pages/guestbook.js
+++ src/pages/guestbook.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 const Button = props =>
 	<button onClick={props.onClick}  type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false" autoComplete="off">
  		{props.label}

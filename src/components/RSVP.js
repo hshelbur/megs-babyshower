4ffb76e83e69852f58d0d899b245bee5214cf43d
hshelbur <howard.shelburne@gmail.com>
2017-10-04 13:40:38 -0400

    styling applied acorss website

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const RSVP = props => (
 			<div className="rsvp">
-				<p className="post-owner">{props.name}</p>
-				<p className="post-rsvp">{props.selectedOption}</p>
+				<p className="post-owner">{props.name}: {props.selectedOption}</p>
 				<p className="post-body">{props.number}</p>
 			</div>
 )
--- src/pages/registry.js
+++ src/pages/registry.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Registry extends Component {
 	render() {
 		return(
-			<div className="registry vertical-align">
+			<div className="registry">
 				<h1>Megan is registered at</h1>
 				<a href="https://amazon.com" className="button" target="_blank" rel="noopener noreferrer">AMAZON</a>
 			</div>
